0f674e66185d48522993ef8e4982ce03e81e9b48
John Dunn <johnwdunn20@gmail.com>
2023-11-04 15:47:10 -0700

    minor changes to auth controller

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -1,22 +1,19 @@
 
-const authcontroller = {
+const authcontroller = {};
 
-    login(req, res, next) {
-        console.log('login controller');
-        next();
-    },
+login(req, res, next) {
+  console.log('login controller');
+  next();
+},
 
-    signup(req, res, next) {
-        console.log('signup controller');
-        next();
-    },
+signup(req, res, next) {
+  console.log('signup controller');
+  next();
+},
 
-    isLoggedIn(req, res, next) {
-        console.log('isLoggedin controller');
-        next();
-    },
-
-}
-
+isLoggedIn(req, res, next) {
+  console.log('isLoggedin controller');
+  next();
+},
 
 module.exports = authcontroller;
