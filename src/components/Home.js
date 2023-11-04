0b43a55d1dbd6c0b33d3bf5c868bfea451e44545
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-11-04 15:41:32 -0700

    text change on header

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
 return (
     <div>
         <h2>Are you Properly Contributing to your FSA?</h2>
-        <p> This handy FSA Calculator will help you estimate your health spending for the year so you can make an informed decision and take maximum advantage of your FSA. Your privacy is important to us, the information you input throughout this tool will not be shared with any third party.</p>
+        <p> This FSA Calculator will help you estimate your health spending for the year so you can make an informed decision and take maximum advantage of your FSA. Your privacy is important to us, the information you input throughout this tool will not be shared with any third party.</p>
         <div>
             <FormSimple />
             <ResultBox />
