6ccceb7187f52f08f6dca8750622c0ecec4e38f8
Prashay <113265973+wadesword89@users.noreply.github.com>
2023-11-05 13:01:01 -0800

    work on sign on component

--- src/components/FormSimple.js
+++ src/components/FormSimple.js
@@ -2,37 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const FormSimple = () => {
-    const [expense23, setExpense23] = useState('');
-    const [expense22, setExpense22] = useState('');
-    const [expense21, setExpense21] = useState('');
+  const [expense23, setExpense23] = useState('');
+  const [expense22, setExpense22] = useState('');
+  const [expense21, setExpense21] = useState('');
 
- const handleSubmit = () => {
+  const handleSubmit = () => {
     // Save data provided by user into a body called data
     // Send a POST request to server with body
- }
-return (
+  };
+  return (
     // <div>FormSimple</div>
     <div>
-        <div className="bg-white p-8 rounded shadow-md w-full sm:w-96">
-          <label className="text-lg font-semibold text-gray-700"> 
-            What were your total average medical expenses in 2023? : 
-          </label>
-          <input
-            type="text"
-            className="w-full border rounded p-2 mt-2 focus:outline-none focus:border-blue-500"
-            value={expense23}
-            onChange={(e) => setExpense23(e.target.value)}
-          />
-        </div>
-        <button 
-          className="g-blue-500 hover:bg-blue-600 text-white font-semibold p-2 mt-4 w-full rounded"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
-
+      <div className="bg-white p-8 rounded shadow-md w-full sm:w-96">
+        <label className="text-lg font-semibold text-gray-700">
+          What were your total average medical expenses in 2023? :
+        </label>
+        <input
+          type="text"
+          className="w-full border rounded p-2 mt-2 focus:outline-none focus:border-blue-500"
+          value={expense23}
+          onChange={(e) => setExpense23(e.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        className="flex w-max justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      >
+        Submit
+      </button>
     </div>
-)
-}
+  );
+};
 
 export default FormSimple;
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,27 +4,32 @@ import FormSimple from './FormSimple';
 import ResultBox from './ResultBox';
 import Button from './Button';
 import MiscInfo from './MiscInfo';
+import SignIn from './SignIn';
 
 const Home = () => {
-
-return (
+  return (
     <div>
-        <h2>Are you Properly Contributing to your FSA?</h2>
-        <p> This FSA Calculator will help you estimate your health spending for the year so you can make an informed decision and take maximum advantage of your FSA. Your privacy is important to us, the information you input throughout this tool will not be shared with any third party.</p>
-        <div>
-            <FormSimple />
-            <ResultBox />
-        </div>
-        <div>Log in to calculate the optimal FSA contribution for you!</div>
-        <div>
-            <Button />
-            <Button />
-        </div>
-        <MiscInfo />
-        
-    </div>
-)
-}
+      <h2>Are you Properly Contributing to your FSA?</h2>
+      <p>
+        {' '}
+        This FSA Calculator will help you estimate your health spending for the
+        year so you can make an informed decision and take maximum advantage of
+        your FSA.{' '}
+      </p>
+      <small>
+        Your privacy is important to us, the information you input throughout
+        this tool will not be shared with any third party.
+      </small>
 
+      <div>
+        <FormSimple />
+        <ResultBox />
+      </div>
+
+      <SignIn />
+      <MiscInfo />
+    </div>
+  );
+};
 
 export default Home;
