5e0269161b557c03c359aea0f49c49ff2b7dbc12
John Dunn <johnwdunn20@gmail.com>
2023-11-06 11:22:47 -0800

    Changes with Prashay's frontend changes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,15 @@ import Protected from './components/Protected';
 
 const App = () => {
     return (
-    <>
+        <>
         <header className="bg-blue-500 p-4 drop-shadow-lg">
-        <h1 className="text-4xl text-white text-center font-extrabold">FlexWise</h1>
+            <h1 className="text-4xl text-white text-center font-extrabold">FlexWise</h1>
             <p className="text-1xl text-white text-center font-bold"> An FSA Optimizer</p>
-        </header> 
-        <main className="bg-gray-100 p-4">
+        </header>
+        <main className="bg-gray-100 p-4" />
         <Home />
-
-    </>
-    );
-  };
+        </>
+    )
+};
 
 export default App;
